e2774cc244b917a4981f13a70ba43d1ab0667f6b
shenry <shenry@handycodejob.com>
2015-10-26 11:55:27 -0700

    send email and set shopify tags for membership

--- url_test.py
+++ url_test.py
@@ -1,10 +1,19 @@
-from flask import Flask, redirect, request, render_template, make_response
-import json
-import requests
+from flask import Flask, redirect, request, render_template
+import shopify
 import stripe
 import os
 
-stripe.api_key = "sk_test_hzmKNeyNVEbyi1BWiTRwRHMe"
+STRIPE_API_SECRET_KEY = os.environ['STRIPE_API_SECRET_KEY']
+STRIPE_API_PUBLIC_KEY = os.environ['STRIPE_API_PUBLIC_KEY']
+SHOPIFY_API_KEY = os.environ['SHOPIFY_API_KEY']
+SHOPIFY_PASSWORD = os.environ['SHOPIFY_PASSWORD']
+URL = "https://%s:%s@upcraft-club.myshopify.com/admin" % (SHOPIFY_API_KEY,
+                                                          SHOPIFY_PASSWORD)
+
+stripe.api_key = STRIPE_API_SECRET_KEY
+shopify.ShopifyResource.set_site(URL)
+
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -12,7 +21,8 @@ app.config['DEBUG'] = True
 @app.route('/', methods=['GET', 'POST'])
 def sub():
     if request.method == 'GET':
-        return render_template("test.html", email=request.args.get('email'))
+        return render_template("test.html", email=request.args.get('email'),
+                               STRIPE_API_PUBLIC_KEY=STRIPE_API_PUBLIC_KEY)
     else:
         print("sub!")
         customer = stripe.Customer.create(
@@ -20,10 +30,14 @@ def sub():
             plan="test_plan",
             metadata={
                 'birthday': request.form['birthday'],
+                'craft-type': request.form['craft-type'],
             },
             email=request.form['email']
         )
         customer.subscriptions.create(plan="test_plan")
+        cust = get_customer(request.form['email'])
+        resp = add_tag(cust, 'Premium')
+        print('customer', customer, 'updated', resp)
         return redirect("http://www.example.com/done", code=302)
 
 
@@ -31,10 +45,31 @@ def sub():
 def stripe_hook():
     event_json = request.get_json()
     obj = event_json['data']['object']
-    if event_json['type'] == "invoice.payment_succeeded":
+    if event_json['type'] == "customer.subscription.created":
         customer = stripe.Customer.retrieve(obj['customer'])
-        print("send discount", customer.email)
+        print("User subbed: ", customer.email)
     return "", 200
+
+
+def get_customer(email):
+    """From and email, retrive customer obj from shopify"""
+    query = shopify.Customer.search(query='email:' + email)
+    for cust in query:
+        if cust.attributes['email'] == email:
+            cust_id = cust.attributes['id']
+    cust = shopify.Customer().find(cust_id)
+    return cust
+
+
+def add_tag(customer, tag='Premium'):
+    """Add a given tag to a customer, then return the result of .save()"""
+    tags = customer.attributes['tags']
+    if tags:
+        tags += ', %s' % tag
+    else:
+        tags = tag
+    customer.attributes.update({'tags': tags})
+    return customer.save()
 
 
 if __name__ == '__main__':
